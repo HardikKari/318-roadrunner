a41d83499664a48d1d1f24941864c04ace3b895d
HardikKari <hardikkari@gmail.com>
2026-09-20 16:40:19 -0500

    Initial RoadRunner project setup

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+
+    [SerializeField]
+    private Vector3 offset = new Vector3(0f, 4f, -7f);
+
+    private void LateUpdate()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        transform.position = target.position + offset;
+    }
+}
